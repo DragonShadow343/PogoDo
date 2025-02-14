702595cabd58a965025c50aaf5b49f9f5c2721ce
branden kennedy <brandenk88@gmail.com>
2025-02-13 18:05:41 -0800

    added boolean lockStatus to the Task class. Did not get rid of setId as i believe we can actually set the Id of the object from the database. I did change priority from 1-5 to be 1-3

--- ogo_mysql_project/src/Task.java
+++ ogo_mysql_project/src/Task.java
@@ -1,4 +1,3 @@
-package src;
 
 public class Task {
     private int id; //primary key
@@ -7,16 +6,19 @@ public class Task {
     private String taskName; //name of task
     private String description; //description of task
     private boolean completionStatus; //true or false if task is completed
+    private boolean lockStatus;
 
 
     //Constructor with all attributes included....
-    public Task(int id, int priority, String dueDate, String taskName, String description, boolean completed) {
+    public Task(int id, int priority, String dueDate, String taskName, String description, boolean completed, boolean lockStatus){ {
         setId(id);
         setPriority(priority);
         setDueDate(dueDate);
         setTaskName(taskName);
         setDescription(description);
         setCompleted(completed);
+        setLockStatus(lockStatus);
+        }
     }
     //Constructor with only taskName and description included.... will have to have id set itself and increment ?
     public Task(String taskName, String description){
@@ -24,60 +26,81 @@ public class Task {
         setDescription(description);
     }
 
+    public void setId(int id){
+        if (id < 0){
+            throw new IllegalArgumentException("ID cannot be negative");
+        }
+        this.id = id;
+    }
     //setters and getters for all attributes
     public int getId(){
         return id;
     }
-    public void setId(int id){
-        if (id < 0){
-            throw new IllegalArgumentException("ID must be greater than 0");
-        }
-        this.id = id;
-    }
+
     public String getDueDate(){
         return dueDate;
     }
+
     public void setDueDate(String dueDate){
         if (dueDate == null || dueDate.isEmpty()){
             throw new IllegalArgumentException("Due date cannot be null");
         }
         this.dueDate = dueDate;
     }
+
     public String getTaskName(){
         return taskName;
     }
+
     public void setTaskName(String taskName){
         if (taskName == null || taskName.isEmpty()){
             throw new IllegalArgumentException("Task name cannot be null");
         }
         this.taskName = taskName;
     }
+
     public String getDescription(){
         return description;
     }
+
     public void setDescription(String description){
         if (description == null || description.isEmpty()){
             throw new IllegalArgumentException("Description cannot be null");
         }
         this.description = description;
     }
+
     public int getPriority(){
         return priorityStatus;
     }
+
     public void setPriority(int priority){
-        if (priority < 1 || priority > 5){
-            throw new IllegalArgumentException("Priority must be between 1 and 5");
+        if (priority < 1 || priority > 3){
+            throw new IllegalArgumentException("Priority must be between 1 and 3");
         }
         this.priorityStatus = priority;
     }
+
     public boolean getCompleted(){
         return completionStatus;
     }
+
     public void setCompleted(boolean completed){
         if (completed != true && completed != false){
             throw new IllegalArgumentException("Completion status must be true or false");
         }
         this.completionStatus = completed;
+    }
+
+    public void setLockStatus(boolean lockStatus){
+        if (lockStatus != true && lockStatus != false){
+            throw new IllegalArgumentException("Lock status must be true or false");
+        }
+        this.lockStatus = lockStatus;
+    }
+
+    public boolean getLockStatus(){
+        return lockStatus;
     }
 
 }
