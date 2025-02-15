778b121b9c1e6ef0b55d075d0cbf756f4ee08d66
Krish Khullar <krishkhullar007@gmail.com>
2025-02-15 13:03:45 -0800

    Merged Frontend-Creation to Merging-login-and-backend

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,6 +1,8 @@
 // Desc: Main App component
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Login from './pages/Login';
 import Register from './pages/Register'
 import Home from './pages/Home';
@@ -11,12 +13,26 @@ import RequireAuth from './RequireAuth';
 
 function App() {
 
+  const [data, setData] = useState('')
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/getData');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
   
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
+        <Route path='/home' data={data} element={<Home />} />
         {/* public routes */}
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
