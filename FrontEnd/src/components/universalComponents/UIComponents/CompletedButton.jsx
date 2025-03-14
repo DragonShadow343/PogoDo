9e54efb367b602517af64f88e9b0516a64020024
Krish Khullar <krishkhullar007@gmail.com>
2025-03-13 19:51:50 -0700

    Changed the UI of the button to be a simple checkbox

--- FrontEnd/src/components/universalComponents/UIComponents/CompletedButton.jsx
+++ FrontEnd/src/components/universalComponents/UIComponents/CompletedButton.jsx
@@ -1,15 +1,16 @@
 import React, { useContext } from "react";
 import TaskContext from "../../../context/TaskProvider";
+import { FiCheck } from "react-icons/fi";
 
 const CompletedButton = ({ taskID, taskCompleted, taskPriority }) => {
     const { toggleTaskCompletion } = useContext(TaskContext);
-    
+
     return (
         <button
             onClick={() => toggleTaskCompletion(taskID, taskCompleted)} // Pass both taskID and current status
-            className={`px-3 py-1 w-28 rounded text-white hover:cursor-pointer 
-                ${taskCompleted ? "bg-[#06D6A0]" : taskPriority === 3 ? "bg-[#EF476F]" : "bg-[#26547C]"}`}>
-            {taskCompleted ? "Completed" : "Mark done"}
+            className={`px-3 py-1 rounded text-white hover:cursor-pointer 
+                ${taskCompleted ? "" : taskPriority === 3 ? "bg-[#EF476F]" : "bg-[#26547C]"}`}>
+            <FiCheck className={`transition ${taskCompleted ? "text-[#06D6A0] " : "text-[#FFFCF9]"}`} />
         </button>
     );
 };
