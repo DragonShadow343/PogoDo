e1e39c8c1c053b2c3b6ee1abb1420c4bdfb6dfd3
Krish Khullar <krishkhullar007@gmail.com>
2025-03-13 20:43:58 -0700

    Made it so that only the admin can see the lock button

--- FrontEnd/src/components/universalComponents/UIComponents/LockButton.jsx
+++ FrontEnd/src/components/universalComponents/UIComponents/LockButton.jsx
@@ -1,15 +1,17 @@
 import React, {useContext} from 'react'
 import TaskContext from "./../../../context/TaskProvider"
+import AuthContext from "./../../../context/AuthProvider"
 import { FiLock, FiUnlock } from "react-icons/fi";
 
 
 const LockButton = ({ taskID, taskLocked, taskPriority }) => {
     const { toggleLockCompletion } = useContext(TaskContext);
-
+    const { auth } = useContext(AuthContext);
+    
   return (
     <button
         onClick={() => toggleLockCompletion(taskID, taskLocked)} // Pass both taskID and current status
-        className="px-3 py-1 rounded text-black hover:cursor-pointer">
+        className={`px-3 py-1 rounded text-black hover:cursor-pointer ${auth.role != "admin"? "hidden" : "block"}`}>
         {taskLocked? <FiLock /> : <FiUnlock />}
     </button>
   )
