7c18239403dbf92f671750dfc546e8025029714a
Krish Khullar <krishkhullar007@gmail.com>
2025-03-22 15:37:45 -0700

    Task board for users created

--- FrontEnd/src/routes/UserRoutes.jsx
+++ FrontEnd/src/routes/UserRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import RequireAuth from '../RequireAuth';
 import Home from '../pages/userPages/Home';
+import TaskBoard from './../pages/userPages/TaskBoard';
 
 import Missing from '../pages/Missing';
 
@@ -9,6 +10,7 @@ const UserRoutes = () => {
         <RequireAuth allowedRoles={['user']}>
             <Routes>
                 <Route path="/" element={<Home/>} />
+                <Route path="/tasks" element={<TaskBoard/>} />
                 <Route path="/home" element={<Home/>} />
                 <Route path="*" element={<Missing />} />
             </Routes>
